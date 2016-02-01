ebee8258a37cc530fda795df3d87d25559073293
Maxwell Salzberg <maxwell@backerkit.com>
2016-01-31 19:14:16 -0800

    update lib to parse name from rel='canonical'

--- build/firefox/data/js/content.js
+++ build/firefox/data/js/content.js
@@ -5,12 +5,16 @@
 		return {
 			run: function () {
 					var path = location.pathname,
+							url = $("*[rel='canonical']").first().attr('href'),
 							prependTarget = $('.container-flex.px2');
 							
+							var parser = document.createElement('a');
+									parser.href = url;
+
 							if(prependTarget.length === 0){
 								prependTarget = $('.i-campaign-page.ng-isolate-scope');
 							}
-					$('<iframe>', {src:'https://www.backerkit.com' + path + '/iframe', frameBorder: "0", width: prependTarget.width(), height:'300px', scrolling:'no'}).prependTo(prependTarget);
+					$('<iframe>', {src:'https://www.backerkit.com' + parser.pathname + '/iframe', frameBorder: "0", width: prependTarget.width(), height:'300px', scrolling:'no'}).prependTo(prependTarget);
 			}
 		};
 	};
